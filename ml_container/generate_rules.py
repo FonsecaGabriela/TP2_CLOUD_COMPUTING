61acb80e1cfde9e7cb647aebea26ba33ce3f8810
FonsecaGabriela <84469754+FonsecaGabriela@users.noreply.github.com>
2024-12-27 01:23:33 -0300

    Update generate_rules.py

--- ml_container/generate_rules.py
+++ ml_container/generate_rules.py
@@ -4,13 +4,13 @@ import pickle
 import os
 
 # Função para carregar e processar dados
-def load_and_process_data(file_path):
+def load_and_process_data(file_url):
     try:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_url)  
         playlists = df.groupby('pid')['track_name'].apply(list).tolist()
         return playlists
     except Exception as e:
-        print(f"Erro ao carregar ou processar o arquivo {file_path}: {e}")
+        print(f"Erro ao carregar ou processar o arquivo {file_url}: {e}")
         return []
 
 # Função para aplicar o algoritmo FP-Growth
@@ -21,31 +21,24 @@ def apply_fpgrowth(playlists, minSupRatio=0.1, minConf=0.5):
     except Exception as e:
         print(f"Erro ao aplicar o algoritmo FP-Growth: {e}")
         return []
-    
+
+# Obter a URL dos dados a partir da variável de ambiente
+file_url = os.getenv("DATA_URL") 
+
+if not file_url:
+    print("A variável de ambiente DATA_URL não está definida.")
+    exit(1)
+
 # Definir o caminho para salvar os arquivos de regras de associação
 output_dir = '../data'
 os.makedirs(output_dir, exist_ok=True)
 
+# Carregar e processar o conjunto de dados
+playlists = load_and_process_data(file_url)
+rules = apply_fpgrowth(playlists)
 
-# Carregar e processar o primeiro conjunto de dados
-playlists1 = load_and_process_data('/home/datasets/spotify/2023_spotify_ds1.csv')
-rules1 = apply_fpgrowth(playlists1)
-
-# Salvar as regras iniciais
-with open(os.path.join(output_dir, 'association_rules_v1.pkl'), 'wb') as f:
-    pickle.dump(rules1, f)
-
-print("Modelo inicial gerado com 2023_spotify_ds1.csv!")
-
-# Carregar e processar o segundo conjunto de dados
-playlists2 = load_and_process_data('/home/datasets/spotify/2023_spotify_ds2.csv')
-rules2 = apply_fpgrowth(playlists2)
-
-# Atualizar as regras de associação com as novas regras, removendo duplicatas
-updated_rules = list({frozenset(rule.items()): rule for rule in rules1 + rules2}.values())
-
-# Salvar as regras atualizadas
+# Salvar as regras de associação
 with open(os.path.join(output_dir, 'association_rules.pkl'), 'wb') as f:
-    pickle.dump(updated_rules, f)
+    pickle.dump(rules, f)
 
-print("Modelo atualizado com 2023_spotify_ds2.csv!")
+print("Modelo gerado e salvo com sucesso!")
